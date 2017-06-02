72f556a978619af19533dc2694d9f74725fd33af
Hack Reactor Student <student@hackreactor.com>
2017-06-02 11:43:29 -0700

    WIP

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// TODO
+var TodoList = (props) => (
+  <li>Halil</li>
+);
+
+
+var App = () => (
+  <div>
+    <h2>My Todo List</h2>
+    <TodoList />
+  </div>
+);
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
